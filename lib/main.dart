6d9c0afd57092211a51da8069a4290ad90222098
m7mdragab <mr341349@gmail.com>
2025-02-04 16:26:48 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CineVault());
+}
+
+class CineVault extends StatelessWidget {
+  const CineVault({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
