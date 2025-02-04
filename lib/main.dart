cde71cdb88ae79265c62a70de13247efe5f09712
m7mdragab <mr341349@gmail.com>
2025-02-04 17:09:34 +0200

    use screenutil to make app responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:cine_vault/config/routes/router.dart';
+import 'package:cine_vault/config/themes/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const CineVault());
@@ -9,6 +12,24 @@ class CineVault extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      fontSizeResolver: (fontSize, screenUtil) {
+        return fontSize * screenUtil.scaleWidth.clamp(0.8, 1.2);
+      },
+      builder: (context, child) {
+        return SafeArea(
+          child: MaterialApp.router(
+            theme: AppThemes.darkTheme,
+            darkTheme: AppThemes.darkTheme,
+            themeMode: ThemeMode.system,
+            debugShowCheckedModeBanner: false,
+            routerConfig: AppRouter.router,
+          ),
+        );
+      },
+    );
   }
 }
